24680c7d19e47cdea32ff19bd44ea7da0ba4893c
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-02-10 12:58:55 -0600

    Tipado estatico

--- hello-world.ts
+++ hello-world.ts
@@ -1,1 +1,12 @@
 console.log('hello world')
+
+let text: string = 'hola'
+
+let bandera: boolean = false
+
+const listaNumeros: number[] = [1, 2, 3]
+
+// no se recominedo utilizarlo salvo sea muy necesario
+const cualquiera: any = false
+
+console.log(text)
