72d7f472756b64e5ee54e5454b3db6c561b4c288
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-02-17 11:39:55 -0600

    Unions types - tipos personalizados

---
+++ src/sesion-2/union-type.ts
@@ -0,0 +1,20 @@
+/**
+ * los types nos sirven para no repetir uniones
+ * o tener el mismo tipo de union en una sola delcaración
+ * y facilitar el mantenimiento del código
+ */
+type StringOrNumber = string | number
+
+function print(value: StringOrNumber, value2: StringOrNumber): StringOrNumber {
+    if (typeof value === 'string') {
+        console.log(value + value2)
+    }
+
+    if (typeof value === 'number' && typeof value2 === 'number') {
+        console.log(value + value2)
+    }
+
+    return value
+}
+
+print('texto1', 2)
