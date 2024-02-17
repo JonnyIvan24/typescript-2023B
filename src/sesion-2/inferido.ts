92ca58aa82ebb4f9e61d25113eb0d01e17f0a78d
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-02-17 10:14:37 -0600

    tipado de variables con valores primitivos y objetos

---
+++ src/sesion-2/inferido.ts
@@ -0,0 +1,7 @@
+// valores inferidos o no mutables
+
+let songName = 'el tucanazo'
+
+// la siguiente asiganción es erronea por que estamos
+// asignando un valor tipo number a uno de tipo string
+// songName = 2 
---
+++ src/sesion-2/tipado-pato.ts
@@ -0,0 +1,11 @@
+// tipado pato
+
+let nameIdObject = { name: 'Juan', id: 1, print() {} }
+// nameIdObject = { id: 2, name: 'Jose' }
+ let nameIdObject2 = { name: 'jose', id: 3, print() {}, select() {} }
+
+ // aignación correcta
+ nameIdObject = nameIdObject
+ // asiganción con error
+ //nameIdObject2 = nameIdObject
+ 
