059f070460f65001aced3d5f8b4c24e7a80c585f
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-02-24 09:59:52 -0600

    Tuplas - tipado de arreglos

---
+++ src/sesion-3/tuplas.ts
@@ -0,0 +1,10 @@
+let tuple1: [string, boolean]
+tuple1 = ['test', true]
+
+/**
+ * TS nos ayuda a tipar arreglos y estos solo podran aceptar
+ * los valores que le hayamos indicado
+ */
+// tuple1 = [false, 'test']
+
+console.log(tuple1)
