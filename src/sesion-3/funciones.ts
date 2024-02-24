f4768c1a573af4a6228ab160d1b0312def1289d7
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-02-24 11:27:50 -0600

    parametros por defecto y tipado de retorno

--- src/sesion-3/funciones.ts
+++ src/sesion-3/funciones.ts
@@ -1,8 +1,9 @@
-const myFunction = (a: number, b?: number) => {
-    if (typeof b === 'undefined') {
-        return a
-    }
+/**
+ * ts no permite parámetros opcionales y por defecto al mismo tiempo
+ * ejemplo: b?: number =  3 
+ */
+const myFunction = (a: number, b: number = 3): number => {
     return a + b
 }
 
-console.log(myFunction(2, 3))
+console.log(myFunction(2))
