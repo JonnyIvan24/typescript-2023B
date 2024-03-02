65f92129f23747b5b9af3b88e3cc093c07131954
JonnyIvan24 <johnny.perez.u@gmail.com>
2024-03-02 11:22:19 -0600

    Encapsulamiento

--- src/sesion-4/clases.ts
+++ src/sesion-4/clases.ts
@@ -1,17 +1,28 @@
 class SimpleClass {
-    id: number
-    name: string
+    private id: number
+    readonly name: string
 
     constructor(name: string) {
         this.id = 1
         this.name = name
     }
 
-    print() {
+    // public no es necesario señalarlo ya que es por defecto
+    public print() {
         console.log('simpleClass.id = ' + this.id)
     }
+
+    /**
+     * las propiedades tipo readonly solo se puede asignar 
+     * valores dentro de constructor de la clase
+     */
+    /* setName(name: string) {
+        this.name = name
+    } */
 }
 
 let simpleClass = new SimpleClass('juan')
+
+// simpleClass.name = ''
 
 simpleClass.print()
